1e0bdb9a03c00690b76ac7d55817ea6a4b87a379
Francisco Ramos <fro.profesional@gmail.com>
2023-03-01 16:31:37 -0800

    Added command pallete

--- src/layouts/app.vue
+++ src/layouts/app.vue
@@ -40,7 +40,7 @@
           <div class="fixed inset-0 bg-gray-600 bg-opacity-75" />
         </TransitionChild>
 
-        <div class="fixed inset-0 z-40 flex  m-1 ">
+        <div class="fixed inset-0 z-40 flex m-1">
           <TransitionChild
             as="template"
             enter="transition ease-in-out duration-300 transform"
@@ -116,11 +116,71 @@
       </Dialog>
     </TransitionRoot>
 
+    <TransitionRoot :show="open" as="template" @after-leave="query = ''" appear>
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
+        <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="ease-in duration-200" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+          <DialogPanel class="mx-auto max-w-2xl transform divide-y divide-gray-500 divide-opacity-20 overflow-hidden rounded-xl bg-gray-900 shadow-2xl transition-all">
+            <Combobox @update:modelValue="onSelect">
+              <div class="relative">
+                <MagnifyingGlassIcon class="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-500" aria-hidden="true" />
+                <ComboboxInput class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-white placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Search..." @change="query = $event.target.value" />
+              </div>
+
+              <ComboboxOptions v-if="query === '' || filteredProjects.length > 0" static class="max-h-80 scroll-py-2 divide-y divide-gray-500 divide-opacity-20 overflow-y-auto">
+                <li class="p-2">
+                  <h2 v-if="query === ''" class="mt-4 mb-2 px-3 text-xs font-semibold text-gray-200">Recent searches</h2>
+                  <ul class="text-sm text-gray-400">
+                    <ComboboxOption v-for="project in query === '' ? recent : filteredProjects" :key="project.id" :value="project" as="template" v-slot="{ active }">
+                      <li :class="['flex cursor-default select-none items-center rounded-md px-3 py-2', active && 'bg-gray-800 text-white']">
+                        <FolderIcon :class="['h-6 w-6 flex-none', active ? 'text-white' : 'text-gray-500']" aria-hidden="true" />
+                        <span class="ml-3 flex-auto truncate">{{ project.name }}</span>
+                        <span v-if="active" class="ml-3 flex-none text-gray-400">Jump to...</span>
+                      </li>
+                    </ComboboxOption>
+                  </ul>
+                </li>
+                <li v-if="query === ''" class="p-2">
+                  <h2 class="sr-only">Quick actions</h2>
+                  <ul class="text-sm text-gray-400">
+                    <ComboboxOption v-for="action in quickActions" :key="action.shortcut" :value="action" as="template" v-slot="{ active }">
+                      <li :class="['flex cursor-default select-none items-center rounded-md px-3 py-2', active && 'bg-gray-800 text-white']">
+                        <component :is="action.icon" :class="['h-6 w-6 flex-none', active ? 'text-white' : 'text-gray-500']" aria-hidden="true" />
+                        <span class="ml-3 flex-auto truncate">{{ action.name }}</span>
+                        <span class="ml-3 flex-none text-xs font-semibold text-gray-400">
+                          <kbd class="font-sans">⌘</kbd>
+                          <kbd class="font-sans">{{ action.shortcut }}</kbd>
+                        </span>
+                      </li>
+                    </ComboboxOption>
+                  </ul>
+                </li>
+              </ComboboxOptions>
+
+              <div v-if="query !== '' && filteredProjects.length === 0" class="py-14 px-6 text-center sm:px-14">
+                <FolderIcon class="mx-auto h-6 w-6 text-gray-500" aria-hidden="true" />
+                <p class="mt-4 text-sm text-gray-200">We couldn't find any projects with that term. Please try again.</p>
+              </div>
+            </Combobox>
+          </DialogPanel>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+
     <!-- Static sidebar for desktop -->
-    <div class="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col m-1 rounded-lg">
+    <div
+      class="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col m-1 rounded-lg"
+    >
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex min-h-0 flex-1 flex-col bg-gray-800 rounded-lg">
-        <div class="flex h-16 flex-shrink-0 items-center bg-gray-900 px-4 rounded-lg">
+        <div
+          class="flex h-16 flex-shrink-0 items-center bg-gray-900 px-4 rounded-lg"
+        >
           <img
             class="h-8 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
@@ -156,9 +216,11 @@
         </div>
       </div>
     </div>
-    
+
     <div class="flex flex-col lg:pl-64">
-      <div class="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white border-gray-200 border-2 rounded-lg m-3">
+      <div
+        class="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white border-gray-200 border-2 rounded-lg m-3"
+      >
         <button
           type="button"
           class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden"
@@ -168,7 +230,7 @@
           <Bars3BottomLeftIcon class="h-6 w-6" aria-hidden="true" />
         </button>
         <div class="flex flex-1 justify-between px-4">
-          <div class="flex flex-1 ">
+          <div class="flex flex-1">
             <form class="flex w-full lg:ml-0" action="#" method="GET">
               <label for="search-field" class="sr-only text-lg">Search</label>
               <div
@@ -185,6 +247,7 @@
                   placeholder="Search"
                   type="search"
                   name="search"
+                  @click="open = true"
                 />
               </div>
             </form>
@@ -246,10 +309,12 @@
 
       <main class="flex-1">
         <div class="py-6">
-
           <div class="flex">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <nav class="flex space-x-4 bg-white border-gray-200 border-2 rounded-lg py-3 px-5" aria-label="Tabs">
+              <nav
+                class="flex space-x-4 bg-white border-gray-200 border-2 rounded-lg py-3 px-5"
+                aria-label="Tabs"
+              >
                 <a
                   v-for="tab in tabs"
                   :key="tab.name"
@@ -267,18 +332,16 @@
             </div>
           </div>
 
-
-
-         <div class="min-h-screen border-gray-200 border-2 rounded-lg m-3 px-3 py-5 mt-10">
-
-            
-              <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
-              </div>
-              <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <slot class="p-1"> </slot>
-              </div>
-         </div >
+          <div
+            class="min-h-screen border-gray-200 border-2 rounded-lg m-3 px-3 py-5 mt-10"
+          >
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+            </div>
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <slot class="p-1"> </slot>
+            </div>
+          </div>
         </div>
       </main>
     </div>
@@ -309,7 +372,18 @@ import {
   XMarkIcon,
 } from "@heroicons/vue/24/outline";
 import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
-
+import {
+  DocumentPlusIcon,
+  FolderPlusIcon,
+  HashtagIcon,
+  TagIcon,
+} from "@heroicons/vue/24/outline";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOptions,
+  ComboboxOption,
+} from "@headlessui/vue";
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
   { name: "Team", href: "#", icon: UsersIcon, current: false },
@@ -331,4 +405,30 @@ const tabs = [
   { name: "Company", href: "#", current: false },
   { name: "Team Members", href: "#", current: true },
 ];
+
+const projects = [
+  { id: 1, name: "Workflow Inc. / Website Redesign", url: "#" },
+  // More projects...
+];
+const recent = [projects[0]];
+const quickActions = [
+  { name: "Add new file...", icon: DocumentPlusIcon, shortcut: "N", url: "#" },
+  { name: "Add new folder...", icon: FolderPlusIcon, shortcut: "F", url: "#" },
+  { name: "Add hashtag...", icon: HashtagIcon, shortcut: "H", url: "#" },
+  { name: "Add label...", icon: TagIcon, shortcut: "L", url: "#" },
+];
+
+const open = ref(false);
+const query = ref("");
+const filteredProjects = computed(() =>
+  query.value === ""
+    ? []
+    : projects.filter((project) => {
+        return project.name.toLowerCase().includes(query.value.toLowerCase());
+      })
+);
+
+function onSelect(item) {
+  window.location = item.url;
+}
 </script>
