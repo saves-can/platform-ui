125c1463d9062207c5e913125d314c011d9e7db3
Francisco Ramos <fro.profesional@gmail.com>
2023-03-01 16:14:34 -0800

    Added general app shell

---
+++ src/layouts/app.vue
@@ -0,0 +1,334 @@
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<template>
+  <!--
+      This example requires updating your template:
+  
+      ```
+      <html class="h-full bg-gray-100">
+      <body class="h-full">
+      ```
+    -->
+  <div>
+    <TransitionRoot as="template" :show="sidebarOpen">
+      <Dialog
+        as="div"
+        class="relative z-40 lg:hidden"
+        @close="sidebarOpen = false"
+      >
+        <TransitionChild
+          as="template"
+          enter="transition-opacity ease-linear duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-gray-600 bg-opacity-75" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 z-40 flex  m-1 ">
+          <TransitionChild
+            as="template"
+            enter="transition ease-in-out duration-300 transform"
+            enter-from="-translate-x-full"
+            enter-to="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leave-from="translate-x-0"
+            leave-to="-translate-x-full"
+          >
+            <DialogPanel
+              class="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800 pt-5 pb-4 rounded-lg"
+            >
+              <TransitionChild
+                as="template"
+                enter="ease-in-out duration-300"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="ease-in-out duration-300"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+              >
+                <div class="absolute top-0 right-0 -mr-12 pt-2">
+                  <button
+                    type="button"
+                    class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    @click="sidebarOpen = false"
+                  >
+                    <span class="sr-only">Close sidebar</span>
+                    <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                  </button>
+                </div>
+              </TransitionChild>
+              <div class="flex flex-shrink-0 items-center px-4">
+                <img
+                  class="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+              </div>
+              <div class="mt-5 h-0 flex-1 overflow-y-auto">
+                <nav class="space-y-1 px-2">
+                  <a
+                    v-for="item in navigation"
+                    :key="item.name"
+                    :href="item.href"
+                    :class="[
+                      item.current
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'group flex items-center rounded-md  px-3 py-3 text-base font-medium',
+                    ]"
+                  >
+                    <component
+                      :is="item.icon"
+                      :class="[
+                        item.current
+                          ? 'text-gray-300'
+                          : 'text-gray-400 group-hover:text-gray-300',
+                        'mr-4 h-6 w-6 flex-shrink-0',
+                      ]"
+                      aria-hidden="true"
+                    />
+                    {{ item.name }}
+                  </a>
+                </nav>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+          <div class="w-14 flex-shrink-0" aria-hidden="true">
+            <!-- Dummy element to force sidebar to shrink to fit close icon -->
+          </div>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+
+    <!-- Static sidebar for desktop -->
+    <div class="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-64 lg:flex-col m-1 rounded-lg">
+      <!-- Sidebar component, swap this element with another sidebar if you like -->
+      <div class="flex min-h-0 flex-1 flex-col bg-gray-800 rounded-lg">
+        <div class="flex h-16 flex-shrink-0 items-center bg-gray-900 px-4 rounded-lg">
+          <img
+            class="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+            alt="Your Company"
+          />
+        </div>
+        <div class="flex flex-1 flex-col overflow-y-auto">
+          <nav class="flex-1 space-y-1 px-2 py-4">
+            <a
+              v-for="item in navigation"
+              :key="item.name"
+              :href="item.href"
+              :class="[
+                item.current
+                  ? 'bg-gray-900 text-white'
+                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'group flex items-center rounded-md px-3 py-3 font-medium',
+              ]"
+            >
+              <component
+                :is="item.icon"
+                :class="[
+                  item.current
+                    ? 'text-gray-300'
+                    : 'text-gray-400 group-hover:text-gray-300',
+                  'mr-3 h-6 w-6 flex-shrink-0',
+                ]"
+                aria-hidden="true"
+              />
+              {{ item.name }} x
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+    
+    <div class="flex flex-col lg:pl-64">
+      <div class="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white border-gray-200 border-2 rounded-lg m-3">
+        <button
+          type="button"
+          class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden"
+          @click="sidebarOpen = true"
+        >
+          <span class="sr-only">Open sidebar</span>
+          <Bars3BottomLeftIcon class="h-6 w-6" aria-hidden="true" />
+        </button>
+        <div class="flex flex-1 justify-between px-4">
+          <div class="flex flex-1 ">
+            <form class="flex w-full lg:ml-0" action="#" method="GET">
+              <label for="search-field" class="sr-only text-lg">Search</label>
+              <div
+                class="relative w-full text-gray-400 focus-within:text-gray-600"
+              >
+                <div
+                  class="pointer-events-none absolute inset-y-0 left-0 flex items-center"
+                >
+                  <MagnifyingGlassIcon class="h-5 w-5" aria-hidden="true" />
+                </div>
+                <input
+                  id="search-field"
+                  class="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
+                  placeholder="Search"
+                  type="search"
+                  name="search"
+                />
+              </div>
+            </form>
+          </div>
+          <div class="ml-4 flex items-center lg:ml-6">
+            <button
+              type="button"
+              class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+
+            <!-- Profile dropdown -->
+            <Menu as="div" class="relative ml-3">
+              <div>
+                <MenuButton
+                  class="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    class="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </MenuButton>
+              </div>
+              <transition
+                enter-active-class="transition ease-out duration-100"
+                enter-from-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-75"
+                leave-from-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <MenuItems
+                  class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <MenuItem
+                    v-for="item in userNavigation"
+                    :key="item.name"
+                    v-slot="{ active }"
+                  >
+                    <a
+                      :href="item.href"
+                      :class="[
+                        active ? 'bg-gray-100' : '',
+                        'block px-4 py-2 text-sm text-gray-700',
+                      ]"
+                      >{{ item.name }}</a
+                    >
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </div>
+        </div>
+      </div>
+
+      <main class="flex-1">
+        <div class="py-6">
+
+          <div class="flex">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <nav class="flex space-x-4 bg-white border-gray-200 border-2 rounded-lg py-3 px-5" aria-label="Tabs">
+                <a
+                  v-for="tab in tabs"
+                  :key="tab.name"
+                  :href="tab.href"
+                  :class="[
+                    tab.current
+                      ? 'bg-indigo-100 text-indigo-700'
+                      : 'text-gray-500 hover:text-gray-700',
+                    'rounded-md px-3 py-2 font-medium',
+                  ]"
+                  :aria-current="tab.current ? 'page' : undefined"
+                  >{{ tab.name }}</a
+                >
+              </nav>
+            </div>
+          </div>
+
+
+
+         <div class="min-h-screen border-gray-200 border-2 rounded-lg m-3 px-3 py-5 mt-10">
+
+            
+              <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+              </div>
+              <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <slot class="p-1"> </slot>
+              </div>
+         </div >
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import {
+  Dialog,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import {
+  Bars3BottomLeftIcon,
+  BellIcon,
+  CalendarIcon,
+  ChartBarIcon,
+  FolderIcon,
+  HomeIcon,
+  InboxIcon,
+  UsersIcon,
+  XMarkIcon,
+} from "@heroicons/vue/24/outline";
+import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
+
+const navigation = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  { name: "Team", href: "#", icon: UsersIcon, current: false },
+  { name: "Projects", href: "#", icon: FolderIcon, current: false },
+  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+  { name: "Documents", href: "#", icon: InboxIcon, current: false },
+  { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
+];
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+const sidebarOpen = ref(false);
+
+const tabs = [
+  { name: "My Account", href: "#", current: false },
+  { name: "Company", href: "#", current: false },
+  { name: "Team Members", href: "#", current: true },
+];
+</script>
