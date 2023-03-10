209fa23ce54cdc6d886dec1a677fa386cc4356a5
Francisco Ramos <fro.profesional@gmail.com>
2023-03-10 06:20:33 -0800

    Formatting

--- src/components/auth/section/form.client.vue
+++ src/components/auth/section/form.client.vue
@@ -63,7 +63,8 @@
               type="submit"
               class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
-              Log in | Sign up with {{ useSMS ? "Phone number" : "Email address" }}
+              Log in | Sign up with
+              {{ useSMS ? "Phone number" : "Email address" }}
             </button>
           </div>
         </form>
