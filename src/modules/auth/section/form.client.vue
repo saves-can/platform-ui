146140526e632fe1ea82be6bd1687bda4b48057c
Francisco Ramos <fro.profesional@gmail.com>
2023-03-10 18:42:29 -0800

    Added Welcome message

--- src/modules/auth/section/form.client.vue
+++ src/modules/auth/section/form.client.vue
@@ -7,11 +7,12 @@
         alt="Your Company"
       />
       <h2
+
         class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
       >
-        Log in or Sign up
+       {{ isLoggedIn ? "Welcome" : "Log in or Sign up" }} 
       </h2>
-      <p class="mt-2 text-center text-sm text-gray-600">
+      <p v-show="!isLoggedIn" class="mt-2 text-center text-sm text-gray-600">
         If you don't have an account we will create it.
         {{ " " }}
       </p>
