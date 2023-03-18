a7da653045af967322a81c619e642ac668007d38
Francisco Ramos <fro.profesional@gmail.com>
2023-03-17 18:50:09 -0700

    [FEATURE] Add i18n module to PLATFORM#77

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import en from "./src/translations/en.json";
+import es from "./src/translations/en.json";
+
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       MAGICLINK_KEY: "pk_live_3537E2E4D3D38899",
       BASE_SITE: "https://clau-dev.com",
-      BASE_API: "https://mlep1rdsr6.execute-api.us-east-1.amazonaws.com"
+      BASE_API: "https://mlep1rdsr6.execute-api.us-east-1.amazonaws.com",
     },
   },
-  // routeRules: {
-  //   '/api/**': { cors: true }, // Add cors headers
-  // },
   srcDir: "src/",
   imports: {
     dirs: ["modules/**/*.store.ts"],
@@ -33,11 +33,22 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "nuxt-icon",
     "nuxt-headlessui",
+    "@nuxtjs/i18n",
   ],
 
   tailwindcss: {
     config: {
       content: ["src/**/**.vue"],
+    },
+  },
+
+  i18n: {
+    locales: ["en", "es"], // used in URL path prefix
+    defaultLocale: "en", // default locale of your project for Nuxt pages and routings
+    vueI18n: {
+      legacy: false,
+      locale: "en",
+      messages: { en, es },
     },
   },
 });
