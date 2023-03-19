de5e6872cc2430bdcefba3e9e5314d793b0b8558
Francisco Ramos <fro.profesional@gmail.com>
2023-03-19 11:22:37 -0700

    Reset channels connection

--- src/modules/test/pusher.client.vue
+++ src/modules/test/pusher.client.vue
@@ -2,12 +2,26 @@
   <div>
     PUSHER!
 
-    <pre>
-      data: {{ data }}
-    </pre>
+    <button
+      class="block m-3 bg-red-500 rounded-lg p-3 text-white"
+      @click="clearMessages"
+    >
+      clearMessages
+    </button>
 
-    <button class="block" @click="execute">execute</button> 
-    <button @click="resetPusher">resetPusher</button>
+    <button
+      class="block m-3 bg-red-500 rounded-lg p-3 text-white"
+      @click="sendMessage"
+    >
+      sendMessage
+    </button>
+
+    <button
+      class="block m-3 bg-red-500 rounded-lg p-3 text-white"
+      @click="setPusher"
+    >
+      setPusher
+    </button>
 
     <pre>
       messages: {{ messages }}
@@ -19,55 +33,54 @@
 import PusherJS from "pusher-js";
 
 let client = ref(null);
-
-const resetPusher = () => {
-  client.value = new PusherJS("8efe3b5d-d99f-4e67-b1e9-39c8e3c49774", {
-    wsHost:
-      "clau-platform-6a998e8f-4990-48ac-9304-5c5b98f6d694.fro-dev-clau.workers.dev",
-    wsPort: 443,
-    wssPort: 443,
-    forceTLS: true,
-    encrypted: true,
-    disableStats: true,
-    enabledTransports: ["ws", "wss"],
-    cluster: "",
-  });
-};
-
-onMounted(() => {
-  client.value = new PusherJS("8efe3b5d-d99f-4e67-b1e9-39c8e3c49774", {
-    wsHost:
-      "clau-platform-6a998e8f-4990-48ac-9304-5c5b98f6d694.fro-dev-clau.workers.dev",
-    wsPort: 443,
-    wssPort: 443,
-    forceTLS: true,
-    encrypted: true,
-    disableStats: true,
-    enabledTransports: ["ws", "wss"],
-    cluster: "",
-  });
-});
-
+let channel = ref(null);
 let messages = ref([]);
-
-client.value = new PusherJS("8efe3b5d-d99f-4e67-b1e9-39c8e3c49774", {
-  wsHost:
-    "clau-platform-6a998e8f-4990-48ac-9304-5c5b98f6d694.fro-dev-clau.workers.dev",
-  wsPort: 443,
-  wssPort: 443,
-  forceTLS: true,
-  encrypted: true,
-  disableStats: true,
-  enabledTransports: ["ws", "wss"],
-  cluster: "",
-});
 
 const { data, execute } = await useAPI("/pusher");
 
 PusherJS.logToConsole = false;
 
-client.value.subscribe("chat-room").bind("new-message", ({ message }) => {
-  logger.info(`Received: ${message}`);
-  messages.value.push(message);
+function subscribeToChannels() {
+  channel.value?.unbind?.();
+  channel.value = client.value.subscribe("chat-room");
+  channel.value.bind("new-message", ({ message }) => {
+    logger.info(`Received: ${message}`);
+    messages.value.push(message);
+  });
+}
+
+function clearMessages() {
+  messages.value = [];
+}
+
+function setPusher() {
+  client.value = new PusherJS("8efe3b5d-d99f-4e67-b1e9-39c8e3c49774", {
+    wsHost:
+      "clau-platform-6a998e8f-4990-48ac-9304-5c5b98f6d694.fro-dev-clau.workers.dev",
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
+    encrypted: true,
+    disableStats: true,
+    enabledTransports: ["ws", "wss"],
+    cluster: "",
+  });
+
+  subscribeToChannels();
+
+  logger.info(`Connect to channels`);
+}
+
+async function sendMessage() {
+  await execute();
+  setPusher();
+}
+
+onMounted(() => {
+  setPusher();
+
+  setInterval(() => {
+    setPusher();
+  }, 10 * 1000);
 });
 </script>
