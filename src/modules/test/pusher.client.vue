fa1d5d3a476508c5d4d05b75f7efcec0e8732a43
Francisco Ramos <fro.profesional@gmail.com>
2023-03-19 10:17:13 -0700

    Added reset pusher connection

--- src/modules/test/pusher.client.vue
+++ src/modules/test/pusher.client.vue
@@ -6,7 +6,8 @@
       data: {{ data }}
     </pre>
 
-    <button @click="execute">execute</button>
+    <button class="block" @click="execute">execute</button> 
+    <button @click="resetPusher">resetPusher</button>
 
     <pre>
       messages: {{ messages }}
@@ -17,16 +18,34 @@
 <script setup>
 import PusherJS from "pusher-js";
 
-let client = new PusherJS("8efe3b5d-d99f-4e67-b1e9-39c8e3c49774", {
-  wsHost:
-    "clau-platform-6a998e8f-4990-48ac-9304-5c5b98f6d694.fro-dev-clau.workers.dev",
-  wsPort: 443,
-  wssPort: 443,
-  forceTLS: true,
-  encrypted: true,
-  disableStats: true,
-  enabledTransports: ["ws", "wss"],
-  cluster: "",
+let client = ref(null);
+
+const resetPusher = () => {
+  client.value = new PusherJS("8efe3b5d-d99f-4e67-b1e9-39c8e3c49774", {
+    wsHost:
+      "clau-platform-6a998e8f-4990-48ac-9304-5c5b98f6d694.fro-dev-clau.workers.dev",
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
+    encrypted: true,
+    disableStats: true,
+    enabledTransports: ["ws", "wss"],
+    cluster: "",
+  });
+};
+
+onMounted(() => {
+  client.value = new PusherJS("8efe3b5d-d99f-4e67-b1e9-39c8e3c49774", {
+    wsHost:
+      "clau-platform-6a998e8f-4990-48ac-9304-5c5b98f6d694.fro-dev-clau.workers.dev",
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
+    encrypted: true,
+    disableStats: true,
+    enabledTransports: ["ws", "wss"],
+    cluster: "",
+  });
 });
 
 let messages = ref([]);
