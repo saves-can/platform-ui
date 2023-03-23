9406bf8f80b13c086325ad8bb6d3c682b820a8df
Francisco Ramos <fro.profesional@gmail.com>
2023-03-22 17:24:59 -0700

    Move app to modules

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,3 @@
 <template>
-  <div>
-    <Head>
-      <Link rel="icon" type="image/x-icon" :href="favicon" />
-    </Head>
-
-    <SchemaOrgOrganization name="Clau" :logo="logo" />
-    <SchemaOrgWebSite name="Clau" />
-    <SchemaOrgWebPage />
-
-    <NuxtLayout>
-      <NuxtPage class="overflow-x-hidden" />
-    </NuxtLayout>
-  </div>
+  <App />
 </template>
-
-<script setup>
-const { BASE_BUCKET } = useRuntimeConfig().public;
-const favicon = `${BASE_BUCKET}/favicon.ico`;
-const logo = `${BASE_BUCKET}/logo.jpg`;
-</script>
